8dc5e6a7177a2ab8a4feb83e17c4b13b75260f8f
ZGennadiy <gennadiy.zinchenko@gmail.com>
2020-06-01 15:57:23 +0300

    add burger menu, add footer

---
+++ src/assets/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const burger = (menuSelector) => {
+        const menuElems = document.querySelectorAll(menuSelector);
+    
+        menuElems.forEach((menu) => {
+            if (window.screen.availWidth >= 992) {
+                menu.classList.remove('menu_vertical');
+            } else {
+                menu.classList.add('menu_vertical');
+            }
+        
+            window.addEventListener('resize', () => {
+                if (window.screen.availWidth >= 992) {
+                    menu.classList.remove('menu_vertical');
+                } else {
+                    menu.classList.add('menu_vertical');
+                }
+            });
+        });
+    };
+
+    burger('.header__wrapper .menu');
+});
